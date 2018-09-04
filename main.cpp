768d60cc64a46ad8401d3886f13c9bc7ca3c6d1e
niggich <ksc@S.local>
2018-09-04 16:52:35 +0300

    Alexey

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
    cout<<"Hello, world!"<<endl;
-   cout<<"My name is Vasya!"<<endl;
-   cout <<"Hello from Di and DArt"<< endl;
+   cout<<"My name is Alexey!"<<endl;
+   cout <<"Hello from Russia"<< endl;
    cout <<"<3"<<endl;
    cout << "Fork!" << endl;
    return 0;
