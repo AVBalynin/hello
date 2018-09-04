9b107885bcc851cd52305b978c55b2ee26f7758f
AV Balynin <alexbalynin@gmail.com>
2018-09-04 22:39:03 +0300

    koroche tut nachalo

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-   cout<<"Hello, world!"<<endl;
-   cout<<"My name is Alexey!"<<endl;
-   cout <<"Hello from Russia"<< endl;
+   cout<<"Play dotka, drink vodka!"<<endl;
+   cout<<"My name is ivan!"<<endl;
+   cout <<"Hello from rushka"<< endl;
    cout <<"<3"<<endl;
    cout << "Fork!" << endl;
    return 0;
